c0187b640ae85831b0a4cd2d06e76b164fe4ac7b
osziszab <osztertagszabolcs@gmail.com>
2019-05-23 14:36:42 +0200

    linq of TimseSlot and DeltaTime added

--- OnlineQueuing/Controllers/UserController.cs
+++ OnlineQueuing/Controllers/UserController.cs
@@ -5,32 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineQueuing.Data;
+using OnlineQueuing.Entities;
+using OnlineQueuing.Services;
 
 namespace OnlineQueuing.Controllers
 {
     public class UserController : Controller
     {
         private readonly ApplicationContext applicationContext;
-        
+        private readonly IAuthService authService;
 
-        public UserController(ApplicationContext application)
+        public UserController(ApplicationContext application, IAuthService authService)
         {
-       
+            this.authService = authService;
             this.applicationContext = application;
         }
-        /*
-                [HttpPost("appintments")]
-                public IActionResult PostNewAppoinment()
-                {
-                    List<DateTime> dataTimes = applicationContext.Users.Include(d=>d.Apointments).ThenInclude(d=>d.D)
 
-                    if ()
-                    {
-                        return Created("", new { message = "Success" });
-                    }
-                    return BadRequest( new { message = "Your request is not valid" });
+            [HttpPost("appointment")]
+             public IActionResult PostNewAppoinment(Appointment appointment)
+             {
+                List<User> appUsers = applicationContext.Users.Select(u => u).ToList();
+
+                List<int> allTimeSlots = appUsers.SelectMany(u => u.Apointments.Select(a => a.TimeSlot)).ToList();
+                List<DateTime> allDatetime = appUsers.SelectMany(d => d.Apointments.Select(t => t.Date.DateTime)).ToList();
+
+                if ()
+                {
+                return Created("", new { message = "Success" });
                 }
-        */
+                return BadRequest( new { message = "Your request is not valid" });
+             }
+       
 
     }
 }
