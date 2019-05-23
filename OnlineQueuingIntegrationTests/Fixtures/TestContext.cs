65b0d92bff9117e7c0bad36c6872a7b83e6bb571
Botond Balogh <botond.balogh8@gmail.com>
2019-05-23 21:39:54 +0200

    Set TestContext

--- OnlineQueuingIntegrationTests/Fixtures/TestContext.cs
+++ OnlineQueuingIntegrationTests/Fixtures/TestContext.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using OnlineQueuing;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace OnlineQueuingIntegrationTests.Fixtures
 {
-    class TestContext
+    class TestContext : IDisposable
     {
+        private TestServer server;
+        public HttpClient Client { get; set; }
+        public readonly ApplicationContext context;
+
+        public TestContext()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Testing")
+                .UseStartup<Startup>()
+                .UseConfiguration(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Testing.json").Build());
+
+            server = new TestServer(builder);
+
+            context = server
+                .Host
+                .Services
+                .GetService(typeof(ApplicationContext)) as ApplicationContext;
+
+            Client = server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            server.Dispose();
+            Client.Dispose();
+        }
     }
 }
