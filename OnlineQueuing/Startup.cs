9a1fe545cc2039d45f0fd320f03c9149df36c92d
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-05-23 15:45:55 +0200

    Create authorization policy for Admin, AdminRequirement.cs/PermissionHandler.cs create, Startup.cs modify

--- OnlineQueuing/Startup.cs
+++ OnlineQueuing/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OnlineQueuing.Data;
+using OnlineQueuing.Helpers;
 using OnlineQueuing.Services;
 
 namespace OnlineQueuing
@@ -37,12 +40,18 @@ namespace OnlineQueuing
         {
             services.AddMvc();
             
-            //services.AddDbContext<ApplicationContext>(builder =>
-            //           builder.UseInMemoryDatabase("InMemoryDatabase"));
-          
             services.AddDbContext<ApplicationContext>(builder =>
-                       builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
-                        .EnableSensitiveDataLogging(true));
+                       builder.UseInMemoryDatabase("InMemoryDatabase"));
+          
+            //services.AddDbContext<ApplicationContext>(builder =>
+            //           builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+            //            .EnableSensitiveDataLogging(true));
+
+            services.AddAuthorization(auth =>
+            {
+            auth.AddPolicy("Admin", policy =>
+                policy.Requirements.Add(new AdminRequirement()));
+            });
 
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationContext>()
@@ -57,9 +66,16 @@ namespace OnlineQueuing
                     {
                         options.ClientId = configuration["Authentication:Google:ClientId"];
                         options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+                        options.AuthorizationEndpoint += "?prompt=consent";
+                        options.AccessType = "offline";
                         options.SaveTokens = true;
+                        options.Scope.Add("https://mail.google.com/");
+                        options.Scope.Add("https://www.googleapis.com/auth/gmail.modify");
+                        options.Scope.Add("https://www.googleapis.com/auth/gmail.compose");
+                        options.Scope.Add("https://www.googleapis.com/auth/gmail.send");
                     });
 
+            services.AddScoped<IAuthorizationHandler, PermissionHandler>();
             services.AddScoped<IAuthService, AuthService>();
         }
 
