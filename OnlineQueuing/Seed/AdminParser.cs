c7620b4fe378196a5947007d0f857beadbbca8e0
JohnLacklan <janos.f96@gmail.com>
2019-05-23 17:26:07 +0200

    Fix style mistakes

--- OnlineQueuing/Seed/AdminParser.cs
+++ OnlineQueuing/Seed/AdminParser.cs
@@ -13,28 +13,28 @@ namespace OnlineQueuing.Seed
 {
     public class AdminParser
     {
-        public ApplicationContext ApplicationContext { get; set; }
-        public string AdminsFromJson { get; set; }
-        public AdminParserDTO AdminParserDTO { get; set; }
-        public IConfiguration Configuration { get; set; }
+        public ApplicationContext applicationContext;
+        public string adminsFromJson;
+        public AdminParserDTO adminParserDTO;
+        public IConfiguration configuration;
 
         public AdminParser(ApplicationContext applicationContext, IConfiguration configuration)
         {
-            this.Configuration = configuration;
-            AdminsFromJson = new StreamReader(configuration["AdminListLocation"]).ReadToEnd();
-            this.ApplicationContext = applicationContext;
-            AdminParserDTO = JsonConvert.DeserializeObject<AdminParserDTO>(AdminsFromJson);
+            this.configuration = configuration;
+            adminsFromJson = new StreamReader(configuration["AdminListLocation"]).ReadToEnd();
+            this.applicationContext = applicationContext;
+            adminParserDTO = JsonConvert.DeserializeObject<AdminParserDTO>(adminsFromJson);
         }
 
         public void FillUpDbWithAdmins()
         {
-            var admins = AdminParserDTO.Admins;
+            var admins = adminParserDTO.Admins;
 
             foreach (var user in admins)
             {
                 User adminToAdd = new User() { Email = user.Email, Role = user.Role, Name = user.Name };
-                ApplicationContext.Add(adminToAdd);
-                ApplicationContext.SaveChanges();
+                applicationContext.Add(adminToAdd);
+                applicationContext.SaveChanges();
             }
         }
     }
