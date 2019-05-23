0bf87bee97ed76898ce79e2308167729ccb2d781
Botond Balogh <botond.balogh8@gmail.com>
2019-05-23 11:04:51 +0200

    Add simple test to check if it works

--- OnlineQueuing-UnitTest/UnitTest1.cs
+++ OnlineQueuing-UnitTest/UnitTest1.cs
@@ -8,7 +8,9 @@ namespace OnlineQueuing_UnitTest
         [Fact]
         public void Test1()
         {
-
+            int n = 5;
+            int result = 5;
+            Assert.Equal(result, n);
         }
     }
 }
