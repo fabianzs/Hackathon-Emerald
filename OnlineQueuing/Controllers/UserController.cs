c5c83edb745a56d0dce3c89cec101e80284dca2d
osziszab <osztertagszabolcs@gmail.com>
2019-05-23 13:42:53 +0200

    database updated

--- OnlineQueuing/Controllers/UserController.cs
+++ OnlineQueuing/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OnlineQueuing.Data;
 
 namespace OnlineQueuing.Controllers
@@ -17,18 +18,19 @@ namespace OnlineQueuing.Controllers
        
             this.applicationContext = application;
         }
+        /*
+                [HttpPost("appintments")]
+                public IActionResult PostNewAppoinment()
+                {
+                    List<DateTime> dataTimes = applicationContext.Users.Include(d=>d.Apointments).ThenInclude(d=>d.D)
 
-        [HttpPost("appintments")]
-        public IActionResult PostNewAppoinment()
-        {
-            List<DateTime> dataTimes = applicationContext.
-
-            if ()
-            {
-                return Created("", new { message = "Success" });
-            }
-            return BadRequest();
-        }
+                    if ()
+                    {
+                        return Created("", new { message = "Success" });
+                    }
+                    return BadRequest( new { message = "Your request is not valid" });
+                }
+        */
 
     }
 }
