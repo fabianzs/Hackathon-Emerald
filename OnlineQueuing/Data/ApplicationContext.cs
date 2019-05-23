8bd6f505763c23503d1e5189cb56b672b43ad132
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-05-23 09:10:41 +0200

    Initialize project

---
+++ OnlineQueuing/Data/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineQueuing.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineQueuing.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
+
---
+++ OnlineQueuing/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineQueuing.Entities
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string Email { get; set; }
+        public string OpenId { get; set; }
+        public string Identity { get; set; }
+    }
+}
