61e8cd418d41c4ff5c2715e283625d1e196b8197
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-05-23 11:22:24 +0200

    Add controller

---
+++ OnlineQueuing/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineQueuing.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateAppointment()
+        {
+            
+            return Created("", new { messageSentTo = "message" });
+        }
+    }
+}
