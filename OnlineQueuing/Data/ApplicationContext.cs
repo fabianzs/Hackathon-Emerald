11138061468a1c190f44e58560a4a49dbd600134
JohnLacklan <janos.f96@gmail.com>
2019-05-23 11:26:57 +0200

    make applicationContext

--- OnlineQueuing/Data/ApplicationContext.cs
+++ OnlineQueuing/Data/ApplicationContext.cs
@@ -12,7 +12,8 @@ namespace OnlineQueuing.Data
         public ApplicationContext(DbContextOptions options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
-
+        public DbSet<Date> Dates { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
