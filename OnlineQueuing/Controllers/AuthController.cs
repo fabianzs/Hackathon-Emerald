5471634f29dd653914afe03b408d951fbbd04f08
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-05-23 13:30:54 +0200

    AuthController.cs modify

--- OnlineQueuing/Controllers/AuthController.cs
+++ OnlineQueuing/Controllers/AuthController.cs
@@ -2,16 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OnlineQueuing.Data;
+using OnlineQueuing.Entities;
+using OnlineQueuing.Services;
 
 namespace OnlineQueuing.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [Authorize]
         [HttpGet("auth")]
         public IActionResult Authenticate()
         {
-            return Ok();
+            string email = authService.GetUserEmail(User);
+            string username = authService.GetUsername(User);
+            User user = authService.SaveUser(email, username);
+            return Ok(user);
+        }
+
+        [Authorize]
+        [HttpGet("accesstoken")]
+        public async Task<IActionResult> GetAccessToken()
+        {
+            string accessToken = string.Empty;
+            if(User.Identity.IsAuthenticated)
+            {
+                accessToken = await HttpContext.GetTokenAsync("access_token");
+            }
+            return Ok(accessToken);
         }
     }
 }
