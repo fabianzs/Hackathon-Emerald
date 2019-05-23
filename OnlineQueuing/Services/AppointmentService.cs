3c0da7648c09da8171fe158612570e05bdd55303
osziszab <osztertagszabolcs@gmail.com>
2019-05-23 16:28:31 +0200

    AppointmentService added

---
+++ OnlineQueuing/Services/AppointmentService.cs
@@ -0,0 +1,26 @@
+﻿using OnlineQueuing.Data;
+using OnlineQueuing.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineQueuing.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly ApplicationContext applicationContext;
+
+        public AppointmentService(ApplicationContext applicationContext)
+        {
+            this.applicationContext = applicationContext;
+        }
+
+        public void CreateAppointment(Appointment appiontment)
+        {
+
+        }
+
+
+    }
+}
---
+++ OnlineQueuing/Services/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using OnlineQueuing.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineQueuing.Services
+{
+    public interface IAppointmentService
+    {
+         void CreateAppointment(Appointment appiontment);
+    }
+}
